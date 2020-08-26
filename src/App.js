396834ca5aeb9a6d9986f1e84b556b6e52d69db6
Kara LeSueur <kara.lesueur@gmail.com>
2020-08-26 11:01:04 -0700

    after break

--- src/App.js
+++ src/App.js
@@ -4,13 +4,30 @@ import './App.css'
 import NavBar from './components/NavBar' 
 
 export default class App extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      spinning: false
+    }
+    this.handleSpin = this.handleSpin.bind(this)
+  }
+
+    handleSpin () {
+      this.setState({
+        spin: !this.state.spinning
+      })
+    }
+
 
 
   render() {
     return(
       <div>
         <NavBar /> 
-        App.js 
+        <div className='square' onClick={() => this.handleSpin()}></div>
+        <div className='heart'></div>
+        <div className='square-click'></div>
       </div>
     )
   }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,18 +2,41 @@ import React, {Component} from 'react'
 import './Navbar.css' 
 
 export default class NavBar extends Component {
+    constructor() {
+        super()
 
+        this.state = {
+            display: false
+        }
+        this.handleSlide = this.handleSlide.bind(this)
+    }
+
+    handleSlide() {
+        this.setState({
+            display: !this.state.display 
+        })
+    }
 
     render() {
+        // console.log(this.state.display)
         return (
             <div>
                 <div className='navbar'>
                     <h1 className='app-name'> NavBar </h1>
+
+                    <div className='navbar-icon' onClick={this.handleSlide}> &#9776; </div>
                     <div className='navbar-button-container'>
                         <button className='navbar-button'> Home </button>
                         <button className='navbar-button'> About </button>
                         <button className='navbar-button'> Contact </button>
                     </div>
+
+                    <div className={this.state.display ? 'dropdown' : 'hidden'}>
+                        <button className='navbar-button'> Home </button>
+                        <button className='navbar-button'> About </button>
+                        <button className='navbar-button'> Contact </button>
+                    </div>
+
                 </div>
             </div>
         )
